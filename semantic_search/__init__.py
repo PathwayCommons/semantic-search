c6dc2f6242a9e11e3afda1b02c2a8a4f4fbdd1f9
johngiorgi <johnmgiorgi@gmail.com>
2021-03-04 12:04:44 -0500

    :bug: Solve weird issue on macOS

--- semantic_search/__init__.py
+++ semantic_search/__init__.py
@@ -1,1 +1,8 @@
+import os
+import platform
+
+# https://github.com/dmlc/xgboost/issues/1715#issuecomment-420305786
+if platform.system() == "Darwin":
+    os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
+
 __version__ = "0.1.0"
