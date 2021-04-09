3d79abe4dbd53d1f235639897ee9f44279f5d1b8
johngiorgi <johnmgiorgi@gmail.com>
2021-04-09 10:28:01 -0400

    :sparkles: Show titles, support URLs

--- semantic_search/demo.py
+++ semantic_search/demo.py
@@ -1,8 +1,26 @@
-import streamlit as st
-import requests
 import json
+from typing import List
 
-URL = "https://semanticsearch.baderlab.org/"
+import requests
+import streamlit as st
+import validators
+
+SEMANTIC_SEARCH_URL = "https://semanticsearch.baderlab.org/"
+PUBMED_BASE_URL = "https://pubmed.ncbi.nlm.nih.gov/"
+
+
+@st.cache(show_spinner=False)
+def get_titles(pmids: List[str]):
+    url = "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications/export/pubtator"
+    json_body = {"pmids": pmids, "concepts": "none"}
+    r = requests.post(url, json=json_body)
+    articles = r.text.strip().split("\n\n")
+    titles = {}
+    for article in articles:
+        pmid, title = article.split("\n")[0].split("|t|")
+        titles[pmid] = title
+    return titles
+
 
 st.sidebar.write(
     """
@@ -22,7 +40,7 @@ st.sidebar.write(
 
 st.sidebar.subheader("Top K")
 top_k = st.sidebar.slider(
-    "Select the number of documents to include in the results", 1, 100, value=(10)
+    "Select the number of documents to include in the results", 1, 50, value=(10)
 )
 
 
@@ -46,22 +64,34 @@ document_ids = [doc_id.strip() for doc_id in document_ids if doc_id]
 """
 # Search
 """
-query_pmid = st.text_input("Enter a query PMID here", help="E.g., 33024307")
+query_pmid = st.text_input(
+    "Enter the PMID or PubMed URL of a paper to query with", help="E.g., 33024307"
+)
 query_pmid = query_pmid.strip()
-
+# Here, we determine if the user copy/pasted a URL and try to parse the PMID out if so.
+# This could likely be handled better with a regex.
+if validators.url(query_pmid):
+    query_pmid = query_pmid.split(PUBMED_BASE_URL)[-1].replace("/", "").strip()
 query = {"query": query_pmid, "documents": document_ids, "top_k": top_k}
 json_body = json.dumps(query)
 
 if query_pmid:
-    response = requests.post(URL, json_body)
+    response = requests.post(SEMANTIC_SEARCH_URL, json_body)
 
-    """
-    # Results
-    ---
-    """
+    pmids = [query_pmid] + [item["uid"] for item in response.json()]
+    titles = get_titles(pmids=pmids)
 
-    for item in response.json():
-        link = f"https://pubmed.ncbi.nlm.nih.gov/{item['uid']}"
-        st.write(f"__PMID__: [{item['uid']}]({link})")
-        st.write(f"__Score__:{item['score']:.4}")
+    st.write("# Results")
+    st.write(f"Displaying the top {top_k} articles most similar to: _{titles[str(query_pmid)]}_")
+    st.write("---")
+
+    for result in response.json():
+        # The cast to str is because the current, deployed version of semantic search
+        # is casting all PMIDs to ints, but this is a bug and will be fixed soon.
+        title = titles.get(str(result["uid"]), "Couldn't find Title.")
+        link = f"https://pubmed.ncbi.nlm.nih.gov/{result['uid']}"
+        st.subheader(title)
+        st.text("")
+        st.write(f"__PMID__: [{result['uid']}]({link})")
+        st.write(f"__Score__: {result['score']:.2%}")
         st.write("---")
