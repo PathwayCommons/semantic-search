bd45d4df9d3869329f95b1eb5c4bb2b027a2c899
Anweshi Anavadya <anweshianavadya@gmail.com>
2021-04-18 23:47:30 -0400

    first ncbi pytest file

--- tests/test_ncbi.py
+++ tests/test_ncbi.py
@@ -4,7 +4,8 @@ from fastapi.exceptions import HTTPException
 
 from semantic_search.ncbi import _medline_to_docs
 
+
 def test_invalid_uid_test():
     with pytest.raises(HTTPException):
-        uid = ['93846392868']
+        uid = ["93846392868"]
         _medline_to_docs(uid)
