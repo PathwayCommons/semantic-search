57fa17ad8070b9e403835ce2c0a3bf8a308d71f2
Anweshi Anavadya <anweshianavadya@gmail.com>
2021-04-19 18:33:14 -0400

    pytest fix as TypeError

--- tests/test_ncbi.py
+++ tests/test_ncbi.py
@@ -1,11 +1,9 @@
 import pytest
 
-from fastapi.exceptions import HTTPException
-
 from semantic_search.ncbi import _medline_to_docs
 
 
 def test_invalid_uid_test():
-    with pytest.raises(HTTPException):
+    with pytest.raises(TypeError):
         uid = ["93846392868"]
         _medline_to_docs(uid)
