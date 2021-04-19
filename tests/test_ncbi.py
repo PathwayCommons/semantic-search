3722fefc51b7684a13665aed7dcfe5c561fc7214
Anweshi Anavadya <anweshianavadya@gmail.com>
2021-04-18 23:46:51 -0400

    first ncbi pytest file

---
+++ tests/test_ncbi.py
@@ -0,0 +1,10 @@
+import pytest
+
+from fastapi.exceptions import HTTPException
+
+from semantic_search.ncbi import _medline_to_docs
+
+def test_invalid_uid_test():
+    with pytest.raises(HTTPException):
+        uid = ['93846392868']
+        _medline_to_docs(uid)
